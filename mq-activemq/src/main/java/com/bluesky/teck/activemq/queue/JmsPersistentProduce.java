2ea7adb55f99a951d946a19341e1ea5b8c081301
mengxianzhu <mengxianzhu@autohome.com.cn>
2020-05-16 18:33:24 +0800

    activeMq persistent demo

--- mq-activemq/src/main/java/com/bluesky/teck/activemq/queue/JmsPersistentProduce.java
+++ mq-activemq/src/main/java/com/bluesky/teck/activemq/queue/JmsPersistentProduce.java
@@ -4,23 +4,36 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 
 import javax.jms.*;
 
-public class JmsProduce {
+/**
+ * 消息可靠性
+ * 1、持久性
+ * 2、事务
+ * 3、签收
+ */
+public class JmsPersistentProduce {
     public static final String ACTIVEMQ_URL = "tcp://localhost:61616";
     public static final String QUEUE_NAME = "queue01";
 
     public static void main(String[] args) throws JMSException {
+        //1创建连接工厂，按照给定的url地址，采用默认用户名和密码
         ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(ACTIVEMQ_URL);
+        //2通过连接工厂，获得连接并启动访问
         Connection connection = factory.createConnection();
         connection.start();
+        //3两个参数，第一个参数事务，第二个参数签收；
         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        //4创建目的地，队列或者主题
         Queue queue = session.createQueue(QUEUE_NAME);
-        //创建消息的生产者
+        //5创建消息的生产者
         MessageProducer producer = session.createProducer(queue);
-        //通过使用messageProducer生产消息发送至mq队列中
+        //设置是否持久化
+        //producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);//非持久化
+        producer.setDeliveryMode(DeliveryMode.PERSISTENT);//持久化
+        //6通过使用messageProducer生产消息发送至mq队列中
         for (int i = 0; i < 3; i++) {
-            //创建消息
+            //7创建消息
             TextMessage textMessage = session.createTextMessage("mag i" + i);
-            //通过messageProducer发送给mq
+            //8通过messageProducer发送给mq
             producer.send(textMessage);
         }
         //关闭资源
