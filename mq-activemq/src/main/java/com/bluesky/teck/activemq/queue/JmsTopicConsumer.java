6bb5b81184e6959be58d94fb2862fc71d75fabf7
mengxianzhu <mengxianzhu@autohome.com.cn>
2020-05-17 09:15:19 +0800

    activemq topic demo

--- mq-activemq/src/main/java/com/bluesky/teck/activemq/queue/JmsTopicConsumer.java
+++ mq-activemq/src/main/java/com/bluesky/teck/activemq/queue/JmsTopicConsumer.java
@@ -10,7 +10,6 @@ public class JmsTopicConsumer {
     public static final String TOPIC_NAME = "topic01";
 
     public static void main(String[] args) throws Exception {
-        //testReceiveMethod();
         testListenMethod();
     }
 
@@ -22,27 +21,11 @@ public class JmsTopicConsumer {
         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
         Topic topic = session.createTopic(TOPIC_NAME);
         MessageConsumer consumer = session.createConsumer(topic);
-        /**
-        consumer.setMessageListener(new MessageListener() {
-            @Override
-            public void onMessage(Message message) {
-                if(message != null && message instanceof TextMessage){
-                    TextMessage textMessage = (TextMessage)message;
-                    try {
-                        System.out.println("receive topic msg:" + textMessage.getText());
-                    } catch (JMSException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
-        */
-        //consumer.setMessageListener((Message message) ->{
         consumer.setMessageListener(message ->{
             if(message != null && message instanceof TextMessage){
                 TextMessage textMessage = (TextMessage)message;
                 try {
-                    System.out.println("receive topic msg:" + textMessage.getText());
+                    System.out.println("consumer1 receive topic msg:" + textMessage.getText());
                 } catch (JMSException e) {
                     e.printStackTrace();
                 }
@@ -54,30 +37,4 @@ public class JmsTopicConsumer {
         connection.close();
         System.out.println("*****finish******");
     }
-//
-//    /**
-//     * receive
-//     * @throws JMSException
-//     */
-//    private static void testReceiveMethod() throws JMSException {
-//        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(ACTIVEMQ_URL);
-//        Connection connection = factory.createConnection();
-//        connection.start();
-//        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-//        Queue queue = session.createQueue(QUEUE_NAME);
-//        MessageConsumer consumer = session.createConsumer(queue);
-//        while(true){
-//            //TextMessage textMessage = (TextMessage)consumer.receive();
-//            TextMessage textMessage = (TextMessage)consumer.receive(3000L);
-//            if(null != textMessage){
-//                System.out.println("receive msg:" + textMessage.getText());
-//            }else {
-//                break;
-//            }
-//        }
-//        consumer.close();
-//        session.close();
-//        connection.close();
-//        System.out.println("*****finish******");
-//    }
 }
--- mq-activemq/src/main/java/com/bluesky/teck/activemq/queue/JmsTopicConsumer02.java
+++ mq-activemq/src/main/java/com/bluesky/teck/activemq/queue/JmsTopicConsumer02.java
@@ -5,6 +5,9 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 import javax.jms.*;
 import java.io.IOException;
 
+/**
+ * JmsTopicConsumer和JmsTopicConsumer02两个订阅topic，均会收到发布消息
+ */
 public class JmsTopicConsumer02 {
     public static final String ACTIVEMQ_URL = "tcp://localhost:61616";
     public static final String TOPIC_NAME = "topic01";
@@ -22,27 +25,11 @@ public class JmsTopicConsumer02 {
         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
         Topic topic = session.createTopic(TOPIC_NAME);
         MessageConsumer consumer = session.createConsumer(topic);
-        /**
-        consumer.setMessageListener(new MessageListener() {
-            @Override
-            public void onMessage(Message message) {
-                if(message != null && message instanceof TextMessage){
-                    TextMessage textMessage = (TextMessage)message;
-                    try {
-                        System.out.println("receive topic msg:" + textMessage.getText());
-                    } catch (JMSException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
-        */
-        //consumer.setMessageListener((Message message) ->{
         consumer.setMessageListener(message ->{
             if(message != null && message instanceof TextMessage){
                 TextMessage textMessage = (TextMessage)message;
                 try {
-                    System.out.println("receive topic msg:" + textMessage.getText());
+                    System.out.println(" consumer2 receive topic msg:" + textMessage.getText());
                 } catch (JMSException e) {
                     e.printStackTrace();
                 }
@@ -54,30 +41,4 @@ public class JmsTopicConsumer02 {
         connection.close();
         System.out.println("*****finish******");
     }
-//
-//    /**
-//     * receive
-//     * @throws JMSException
-//     */
-//    private static void testReceiveMethod() throws JMSException {
-//        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(ACTIVEMQ_URL);
-//        Connection connection = factory.createConnection();
-//        connection.start();
-//        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-//        Queue queue = session.createQueue(QUEUE_NAME);
-//        MessageConsumer consumer = session.createConsumer(queue);
-//        while(true){
-//            //TextMessage textMessage = (TextMessage)consumer.receive();
-//            TextMessage textMessage = (TextMessage)consumer.receive(3000L);
-//            if(null != textMessage){
-//                System.out.println("receive msg:" + textMessage.getText());
-//            }else {
-//                break;
-//            }
-//        }
-//        consumer.close();
-//        session.close();
-//        connection.close();
-//        System.out.println("*****finish******");
-//    }
 }
