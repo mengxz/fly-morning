1164ec9acca01f3c9b12531fab984f00b0fa65a2
meng <215495667@qq.com>
2020-10-18 22:28:27 +0800

    TestSocket linux查看底层调用

--- java-core/src/main/java/com/bluesky/tech/io/TestSocket.java
+++ java-core/src/main/java/com/bluesky/tech/io/TestSocket.java
@@ -7,6 +7,37 @@ import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ * https://www.bilibili.com/video/BV11K4y1C7rm?p=1
+ * IO多路复用原理分析
+ * nio,epoll,多路复用
+ * Linux抓取程序对内核发生的系统调用，输出到指定文件；
+ * strace -ff -o ./xxxx java TestSocket
+ *
+ * jps查询进程号 2878
+ * cd /proc/2878
+ * cd /proc/2878/task
+ * cd /proc/2878/fd   linux一切皆文件【文件描述符】0-标准输入；1-标准输出；2-错误输出；
+ * 查询网络端口
+ * netstat -natp
+ *
+ *
+ *linux程序nc连接socket
+ *nc localhost 8090
+ *查询网络端口
+ *netstat -natp
+ *cd /proc/2878/
+ *ll
+ *文件描述符增加一个新的文
+ *
+ *
+ * less
+ * /socket.*5$
+ *
+ * 查看linux命令帮助
+ * man accept
+ * man 2 bind
+ */
 public class TestSocket {
     public static void main(String[] args)throws Exception {
         ServerSocket serverSocket = new ServerSocket(8090);
